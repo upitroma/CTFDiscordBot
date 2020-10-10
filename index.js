da6cccc133a2d92dc1fb8f3f7b48c9877903d2a1
upitroma <allen@rudolphweb.com>
2020-10-10 15:11:46 -0400

    scoreboard works

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ client.login(config.TOKEN);
 
 
 class Hacker{
-    constructor(userId){
+    constructor(userId, username){
         this.userId=userId;
         this.score=0;
         this.achievements=[];
+        this.username=username;
     }
 }
 hackers=[];
@@ -32,35 +33,75 @@ client.on("message", (message) => {
             if(message.content==strings.flags[i]){
                 isValidFlag=true
 
-                //reward hacker
+                //flag is valid
                 hackerExists=false
-                for(i=0;i<hackers.length;i++){
-                    h=hackers[i]
+                for(j=0;j<hackers.length;j++){
+                    h=hackers[j]
                     if(h.userId==message.author.id){
                         hackerExists=true
-                        h.score++;
-                        message.channel.send(/*"<#"+message.channel.id+">"+*/"  <@"+message.author.id+"> "+strings.responses[i]+"\n"+"current score: "+h.score);
+
+                        allreadyGotFlag=false
+
+                        for(a=0;a<h.achievements.length;a++){
+                            if(h.achievements[a]==strings.achievements[i]){
+                                allreadyGotFlag=true;
+                            }
+                        }
+
+                        if(allreadyGotFlag){
+                            client.channels.cache.get('759916479835275305').send(/*"<#"+message.channel.id+">"+*/"  <@"+message.author.id+"> You already submitted that flag. No points for you.");
+                        }
+                        else{
+                            h.score+=strings.pointValues[i];
+                            h.achievements.push(strings.achievements[i]);
+                            client.channels.cache.get('759916479835275305').send(/*"<#"+message.channel.id+">"+*/"  <@"+message.author.id+"> "+strings.responses[i]+"\n+"+strings.pointValues[i]+"\ncurrent score: "+h.score);
+                        }
+
+                        
                     }
                 }
                 if(!hackerExists){
-                    h=new Hacker(message.author.id)
-                    h.score=1;
+                    h=new Hacker(message.author.id, message.author.username)
                     hackers.push(h)
-                    message.channel.send(/*"<#"+message.channel.id+">"+*/"  <@"+message.author.id+"> "+strings.responses[i]+"\n"+"current score: "+h.score);
+
+                    h.score+=strings.pointValues[i];
+                    h.achievements.push(strings.achievements[i]);
+                    client.channels.cache.get('759916479835275305').send(/*"<#"+message.channel.id+">"+*/"  <@"+message.author.id+"> "+strings.responses[i]+"\n+"+strings.pointValues[i]+"\ncurrent score: "+h.score);
                     console.log("new hacker added! username: "+message.author.username)
                 }
-
-
             }
         }
 
 
         if(!isValidFlag){
-            message.channel.send("<@"+message.author.id+"> You didn't say the magic word.");
+            client.channels.cache.get('759916479835275305').send("<@"+message.author.id+"> You didn't say the magic word.");
         }
 
         message.delete();
         return
     }
 
+    else if(message.content=="!score"){
+        printScore()
+    }
+
 });
+
+
+function printScore(){
+
+    hackers.sort(function (a, b) {
+        return a.score - b.score;
+    }).reverse();
+
+
+    scoreboard="";
+
+    for(i=0;i<hackers.length;i++){
+        scoreboard+=hackers[i].username+" Score: "+hackers[i].score+"\n";//+"\nAchievements:\n";
+        for(j=0;j<hackers[i].achievements.length;j++){
+            scoreboard+="\t"+hackers[i].achievements[j]+"\n";
+        }
+    }
+    client.channels.cache.get('759916479835275305').send(scoreboard);
+}
